bb4d442d98d0bdac04798d743019e04b6a4fa902
hennssy <100162290+hennssy@users.noreply.github.com>
2023-03-12 15:44:27 +0500

    update

--- C++/authsystem/main.cpp
+++ C++/authsystem/main.cpp
@@ -3,7 +3,6 @@
 #include <iomanip>
 #include <windows.h>
 #include <fstream>
-#include <cstdio>
 
 using namespace std;
 
@@ -43,431 +42,224 @@ string passwordadd(string insert)
 	return password;
 }
 
-void admindel()
+void userdel(string insert)
 {
-	cin.ignore();
-	system("cls");
-	cout << setw(50) << "Удаление Администратора\n\n";
-	string insert, login, password, chose, check;
-	int flag = 1;
-	cout << "Введите логин и пароль Администратора, которого необходимо удалить, в формате логин:пароль: ";
-	getline(cin, insert);
-	insert = formatcheck(insert);
-	ifstream superuser("superusers.txt");
+	ifstream users("users.txt");
+	ofstream temp("temp.txt");
+	int position;
+	string check;
 	
-	while(getline(superuser, check))
+	while(getline(users, check))
 	{
-		while(getline(superuser, check))
-		{
-			if(check == insert) flag = 0;
-		}
+		position = check.find(":");
 		
-		while(flag)
-		{
-			cout << "Указанного администратора не существует\n";
-			cout << "Повторите попытку: ";
-			getline(cin, insert);
-			insert = formatcheck(insert);
-			superuser.clear();
-			superuser.seekg(0);
-			
-			while(getline(superuser, check))
-			{
-				if(check == insert) flag = 0;
-			}
-		}
+		if(check.find(insert) != string::npos) continue;
+		else temp << check << "\n"; 
 	}
 	
-	login = loginadd(insert);
-	password = passwordadd(insert);
-	cout << "Вы уверены, что хотите удалить Администратора " << login << "?\n";
-	getline(cin, chose);
-	
-	if(chose == "Да" || chose == "да")
-	{
-		cout << "Администратор успешно удалён";
-	}
-	
-	if(chose == "Нет" || chose == "нет")
-	{
-		cout << "Возвращаемся в меню";
-		Sleep(3000);
-		return void();
-	}
-	
-	ofstream temp;
-	temp.open("temp.txt");
-	superuser.clear();
-	superuser.seekg(0);
-	
-	while(getline(superuser, check))
-	{
-		if(check == insert) continue;
-		else temp << check << "\n";
-	}
-	
+	users.close();
 	temp.close();
-	superuser.close();
-	remove("superusers.txt");
-	rename("temp.txt", "superusers.txt");
-	Sleep(3000);
-}
-
-void adminadd()
-{
-	cin.ignore();
-	system("cls");
-	cout << setw(50) << "Добавление Администратора\n\n";
-	string login, password, insert, chose, check;
-	int flag;
-	cout << "Введите логин и пароль администратора в формате логин:пароль: ";
-	getline(cin, insert);
-	insert = formatcheck(insert);
-	ofstream superusers;
-	ifstream superuser("superusers.txt");
-	superusers.open("superusers.txt", ios::app);
-	
-	while(getline(superuser, check))
-	{
-		if(check == insert)
-		{
-			flag = 1;
-			
-			while(flag)
-			{
-				cout << "Указанный Администратор уже существует\n";
-				cout << "Вы хотите продолжить?(да/нет)\n";
-				string continuechose;
-				getline(cin, continuechose);
-				
-				if(continuechose == "Да" || continuechose == "да")
-				{
-					cout << "Введите логин и пароль: ";
-					getline(cin, insert);
-					insert = formatcheck(insert);
-					
-					if(check != insert)
-					{
-						flag = 0;
-						superuser.clear();
-						superuser.seekg(0);
-					}
-				}
-				
-				if(continuechose == "Нет" || continuechose == "нет") 
-				{
-					cout << "Возвращаемся в меню";
-					flag = 0;
-					Sleep(3000);
-					system("cls");
-					return void();	
-				} 									
-			}
-		}
-	}
-	
-	login = loginadd(insert);
-	password = passwordadd(insert);
-	cout << "Добавить Администратора " << login << " с паролем " << password << "?(да/нет)\n";
-	getline(cin, chose);
-	
-	if(chose == "Да" || chose == "да")
-	{
-		if(superusers.is_open())
-		{
-			superusers << insert << "\n";
-		}
-	
-		superusers.close();
-		
-		cout << "\nАдминистратор успешно добавлен, возвращаемся в меню";
-		Sleep(3000);
-		return void();
-	}
-	
-	if(chose == "Нет" || chose == "нет")
-	{
-		cout << "Вы отказались добавлять администратора, возвращаемся в меню";
-		Sleep(3000);
-		system("cls");
-		return void();
-	}
-}
-
-void useradd()
-{
-	cin.ignore();
-	system("cls");
-	cout << setw(50) << "Добавление Пользователя\n\n";
-	string login, password, insert, chose, check;
-	int flag;
-	cout << "Введите логин и пароль нового Пользователя в формате логин:пароль: ";
-	getline(cin, insert);
-	insert = formatcheck(insert);
-	ofstream users;
-	ifstream user("users.txt");
-	users.open("users.txt", ios::app);
-	
-	while(getline(user, check))
-	{
-		if(check == insert)
-		{
-			flag = 1;
-			
-			while(flag)
-			{
-				cout << "Указанный Пользователь уже существует\n";
-				cout << "Вы хотите продолжить?(да/нет)\n";
-				string continuechose;
-				getline(cin, continuechose);
-				
-				if(continuechose == "Да" || continuechose == "да")
-				{
-					cout << "Введите логин и пароль: ";
-					getline(cin, insert);
-					insert = formatcheck(insert);
-					
-					if(check != insert)
-					{
-						flag = 0;
-						user.clear();
-						user.seekg(0);
-					}
-				}
-				
-				if(continuechose == "Нет" || continuechose == "нет") 
-				{
-					cout << "Возвращаемся в меню";
-					flag = 0;
-					Sleep(3000);
-					system("cls");
-					return void();	
-				} 									
-			}
-		}
-	}
-	
-	login = loginadd(insert);
-	password = passwordadd(insert);
-	cout << "Добавить Пользователя " << login << " с паролем " << password << "?(да/нет)\n";
-	getline(cin, chose);
-	
-	if(chose == "Да" || chose == "да")
-	{
-		if(users.is_open())
-		{
-			users << insert << "\n";
-		}
-	
-		users.close();
-		
-		cout << "\nПользователь успешно добавлен, возвращаемся в меню";
-		Sleep(3000);
-		return void();
-	}
-	
-	if(chose == "Нет" || chose == "нет")
-	{
-		cout << "Вы отказались добавлять Пользователя, возвращаемся в меню";
-		Sleep(3000);
-		system("cls");
-		return void();
-	}
-}
-
-void userdel()
-{
-	cin.ignore();
-	system("cls");
-	cout << setw(50) << "Удаление Пользователя\n\n";
-	string insert, login, password, chose, check;
-	int flag = 1;
-	cout << "Введите логин и пароль Пользователя, которого необходимо удалить, в формате логин:пароль: ";
-	getline(cin, insert);
-	insert = formatcheck(insert);
-	ifstream user("users.txt");
-	
-	while(getline(user, check))
-	{
-		while(getline(user, check))
-		{
-			if(check == insert) flag = 0;
-		}
-		
-		while(flag)
-		{
-			cout << "Указанного Пользователя не существует\n";
-			cout << "Повторите попытку: ";
-			getline(cin, insert);
-			insert = formatcheck(insert);
-			user.clear();
-			user.seekg(0);
-			
-			while(getline(user, check))
-			{
-				if(check == insert) flag = 0;
-			}
-		}
-	}
-	
-	login = loginadd(insert);
-	password = passwordadd(insert);
-	cout << "Вы уверены, что хотите удалить Пользователя " << login << "?\n";
-	getline(cin, chose);
-	
-	if(chose == "Да" || chose == "да")
-	{
-		cout << "Пользователь успешно удалён";
-	}
-	
-	if(chose == "Нет" || chose == "нет")
-	{
-		cout << "Возвращаемся в меню";
-		Sleep(3000);
-		return void();
-	}
-	
-	ofstream temp;
-	temp.open("temp.txt");
-	user.clear();
-	user.seekg(0);
-	
-	while(getline(user, check))
-	{
-		if(check == insert) continue;
-		else temp << check << "\n";
-	}
-	
-	temp.close();
-	user.close();
 	remove("users.txt");
 	rename("temp.txt", "users.txt");
-	Sleep(3000);
+}
+
+void useradd(string insert, string flag)
+{
+	ofstream users("users.txt", ios::app);
+	if(flag == "Да" || flag == "да") users << insert << " SU\n";
+	else if(flag == "Нет" || flag == "нет") users << insert << "\n"; 
+	
+	else
+	{
+		cout << "\nУказан неправильный ответ, возврат в меню...";
+		return void();
+	}
+	
+	cout << "\nПользователь успешно добавлен\nВозврат в меню...";
+	users.close();
+}
+
+int rolecheck(string insert)
+{
+	string check;
+	int flag = 0, position;
+	ifstream users("users.txt");
+	
+	while(getline(users, check))
+	{
+		position = check.find(":");
+		
+		if(check.find(insert) != string::npos)
+		{
+			if(check.find("SU", position) != string::npos) 
+			{
+				flag = 1;
+				break;
+			}
+			
+			else 
+			{
+				flag = 0;
+				break;
+			}
+		}
+		
+		else flag = 2;
+	}
+	
+	users.close();
+	return flag;
+}
+
+void menudel(string insert)
+{
+	string adminchoise;
+	cout << "\nВы хотите его удалить?\n\nВаш выбор(да/нет):";
+	getline(cin, adminchoise);
+			
+	if(adminchoise == "Да" || adminchoise == "да")
+	{
+		userdel(insert);
+		cout << "\nУдаление успешно завершено\nВозврат в меню...";
+		Sleep(3000);
+	}
+			
+	else if(adminchoise == "Нет" || adminchoise == "нет")
+	{
+		cout << "\nВозврат в меню...";
+		Sleep(3000);
+		return void();
+	}
+			
+	else
+	{
+		cout << "\nУказан неправильный ответ, возврат в меню...";
+		Sleep(3000);
+		return void();
+	}
+}
+
+void menuadd(string insert)
+{
+	string adminchoise, flag;
+	cout << "\nВы хотите его добавить?\n\nВаш выбор(да/нет): ";
+	getline(cin, adminchoise);
+			
+	if(adminchoise == "Да" || adminchoise == "да")
+	{
+		cout << "\nДать пользователю права администратора?\n\nВаш выбор: ";
+		getline(cin, flag);
+		useradd(insert, flag);
+		Sleep(3000);
+	}
+			
+	else if(adminchoise == "Нет" || adminchoise == "нет")
+	{
+		cout << "\nВозврат в меню...";
+		Sleep(3000);
+		return void();
+	}
+			
+	else
+	{
+		cout << "\nУказан неправильный ответ, возврат в меню...";
+		Sleep(3000);
+		return void();
+	}
+}
+
+void userchange(string insert)
+{
+	system("cls");
+	cout << setw(50) << "Редактирование пользователей\n\n";
+	cout << "Проверка " << insert << "...\n";
+	int flag = rolecheck(insert);
+	string login = loginadd(insert), password = passwordadd(insert);
+	
+	switch(flag)
+	{
+		case 0:
+			cout << "\nПользователь " << login << " существует и имеет обычные права";
+			menudel(insert);
+			break;
+		case 1: 
+			cout << "\nПользователь " << login << " существует и имеет права администратора";
+			menudel(insert);
+			break;
+		case 2:
+			cout << "\nУказанный пользователь отсутствует";
+			menuadd(insert);
+			break;
+	}
 }
 
 void superuser()
 {
-	int chose;
+	int choise;
 	
 	do
 	{
-		cout << setw(75) << "Вы вошли в систему как пользователь с правами Администратора\n"
-			"Вам доступны следующие функции: \n"
-			"1. Изменение списка Администраторов\n"
-			"2. Изменение списка Пользователей\n"
-			"3. Получить доступ к файлу\n"
-			"4. Возврат в меню аутентификации\n\n"
-			"Выберите функцию из списка: ";
-		cin >> chose;
-		int adminchose;
-		
-		if(chose == 3)
-		{
-			system("cls");
-			system("userpass.txt");
-		}
-		
-		do
-		{
-			switch(chose)
-			{
-				case 1:
-					system("cls");
-					cout << setw(50) << "Изменение списка Администраторов\n";
-					cout << "1. Добавление нового Администратора\n"
-						"2. Удаление уже существующего Администратора\n"
-						"3. Возврат в предыдущее меню\n\n"
-						"Выберите функцию из списка: ";
-					cin >> adminchose;
-					if(adminchose == 1) adminadd();
-					if(adminchose == 2) admindel();
-					break;
-				
-				case 2:
-					system("cls");
-					cout << setw(50) << "Изменение списка Пользователей\n";
-					cout << "1. Добавление нового Пользователя\n"
-						"2. Удаление уже существующего Пользователя\n"
-						"3. Возврат в предыдущее меню\n\n"
-						"Выберите функцию из списка: ";
-					cin >> adminchose;
-					if(adminchose == 1) useradd();
-					if(adminchose == 2) userdel();
-					break;
-
-				case 3:
-					adminchose = 3;
-					break;
-
-				case 4:
-					system("cls");
-					return void();
-					break;
-			} 
-			system("cls");
-		} while (adminchose != 3);
 		system("cls");
-	} while (chose != 4);
-}
-
-int auth(string insert)
-{
-	ifstream users("users.txt");
-	string check;
-	int flag = 2;
+		cout << setw(50) << "Меню администратора\n\n";
+		cout << "Доступны следующие функции:\n1 - Изменение списка пользователей\n2 - Открытие файла\n3 - Выход из системы\n\n";
+		cout << "Ваш выбор: ";
+		cin >> choise;
+		string insert;
+		
+		switch(choise)
+		{
+			case 1: 
+				cout << "\nВведите логин и пароль пользователя: ";
+				cin.ignore();
+				getline(cin, insert);
+				insert = formatcheck(insert);
+				userchange(insert);
+				break;
+			case 2:
+				cout << "\nОткрытие файла...";
+				system("userpass.txt");
+				cout << "\nРабота с файлом завершена...";
+				Sleep(3000);
+				system("cls");
+				break;
+		}
+	} while (choise != 3);
 	
-	while(getline(users, check))
-	{
-		if(check == insert) flag = 0;
-	}
-	
-	users.close();
-	ifstream superusers("superusers.txt");
-	
-	while(getline(superusers, check))
-	{
-		if(check == insert) flag = 1;
-	}
-	
-	superusers.close();
-	return flag;
+	return void();
 }
 
 int main() 
 {
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
-	cout << setw(50) << "Авторизация\n";
-	cout << "Введите логин и пароль: ";
 	string insert;
+	int flag;
+	cout << setw(50) << "Авторизация\n\n";
+	cout << "Введите логин и пароль: ";
 	getline(cin, insert);
 	insert = formatcheck(insert);
-	int flag = auth(insert);
-
-	if(flag == 0)
-	{
-		cout << "Вы авторизовались как обычный Пользователь\n"
-			"Открытие файла...";
-		Sleep(3000);
-		system("userpass.txt");
-		system("cls");
-	}
+	flag = rolecheck(insert);
 	
-	if(flag == 1)
+	switch(flag)
 	{
-		cout << "Вы авторизовались с правами Администратора\n"
-			"Открытие меню...";
-		Sleep(3000);
-		system("cls");
-		superuser();
+		case 0:
+			cout << "\nВы вошли в систему как обычный пользователь\n";
+			cout << "Открытие файла...";
+			Sleep(3000);
+			system("userpass.txt");
+			cout << "\n\nРабота с системой завершена. Всего доброго!";
+			Sleep(3000);
+			break;
+		case 1:
+			cout << "\nВы вошли в систему как администратор\nПереход в меню...";
+			Sleep(3000);
+			superuser();
+			cout << "\n\nРабота с системой завершена. Всего доброго!";
+			Sleep(3000);
+			break;
+		case 2:
+			cout << "\nУказанный пользователь не внесён в систему\n";
+			cout << "Обратитесь к администратору";
+			break;
 	}
-	
-	if(flag == 2)
-	{
-		cout << "Указанного пользователя не существует\n"
-			"Повторите попытку или обратитесь к Администратору";
-	}
-	
-	cout << "Работа с системой успешно завершена. Хорошего дня!";
-	return 0;
 }
